aed500fd90c7f4d365492a7704a68f044ffd003c
Luka <lukam@nym.hush.com>
2019-04-22 16:57:33 +0200

    Initial commit

---
+++ precache-manifest.72e6f213944b09393ee8dfbabc982805.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7ba68d3dfa358ebe3de0",
+    "url": "/static/js/main.0ac055ab.chunk.js"
+  },
+  {
+    "revision": "e90574d74274c7fb4d49",
+    "url": "/static/js/2.c39fed63.chunk.js"
+  },
+  {
+    "revision": "7ba68d3dfa358ebe3de0",
+    "url": "/static/css/main.e3f1d5b0.chunk.css"
+  },
+  {
+    "revision": "778a7055841bf7a31fb76c45d814a967",
+    "url": "/index.html"
+  }
+];
